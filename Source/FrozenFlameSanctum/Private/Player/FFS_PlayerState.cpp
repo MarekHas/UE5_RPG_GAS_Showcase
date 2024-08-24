69203a5fa23d7dbff8269df0b884b1e535b8b436
MarekHas <m.hasulak@gmail.com>
2024-08-24 16:59:09 +0200

    addd ffs player state

---
+++ Source/FrozenFlameSanctum/Private/Player/FFS_PlayerState.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/FFS_PlayerState.h"
+
+AFFS_PlayerState::AFFS_PlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
